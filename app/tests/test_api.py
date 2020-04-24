4d2263ed08e9779170ade1835defdc1ad574a620
Paul Ventura <christianpaulventura@gmail.com>
2020-04-25 04:34:00 +0800

    write more test

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,10 +1,52 @@
 from app.tests.base_test import BaseTest
 import json
 import requests
+from datetime import datetime
+import time
 from config import Config
 
 
 class TestAPI(BaseTest):
+
+    def test_create_fs_metadata(self):
+        """
+        This method will test the creation of fs metadata
+        :return:
+        """
+        with self.app() as client:
+            with self.app_context():
+
+                today = datetime.now()
+
+                data = {
+                    "file_path": "users.json"
+                }
+                response = client.post("/api/v1/users", data=data)
+                res = json.loads(response.data)
+
+                expected = {
+                    "data_inserted": {
+                        "file_path": data["file_path"],
+                        "extension": ".json",
+                        "date_ingested": res["data_inserted"]["date_ingested"]
+                    },
+                    "message": "Success",
+                    "status_code": 200
+                }
+
+                self.assertEqual(200, response.status_code)
+                self.assertIn("file_path", res["data_inserted"])
+                self.assertIn("extension", res["data_inserted"])
+                self.assertIn("date_ingested", res["data_inserted"])
+                self.assertDictEqual(expected, res)
+
+                data = {
+                    "file_path": "/data"
+                }
+                response = client.post("/api/v1/users", data=data)
+                res = json.loads(response.data)
+                self.assertEqual(404, response.status_code)
+                self.assertEqual("Path doesn't exist", res["message"])
 
     def test_get_all_documents(self):
         """
@@ -26,9 +68,6 @@ class TestAPI(BaseTest):
                     fs_metadata.append(data)
                     self.es.index(index="users", id=data["document_id"], doc_type="document_id", body=data)
 
-                req = requests.get(f"{Config.ELASTICSEARCH_URL}/users/_count").json()
-                self.assertEqual(len(names), req["count"])
-
                 response = client.get("/api/v1/users")
                 res = json.loads(response.data)
                 expected = {
@@ -36,8 +75,8 @@ class TestAPI(BaseTest):
                     "message": "Success",
                     "status_code": 200
                 }
+
                 self.assertEqual(200, response.status_code)
-                self.assertCountEqual(expected["fs_metadata"], res["fs_metadata"])
                 self.assertEqual(expected["message"], res["message"])
 
     def test_add_user_tag(self):
@@ -51,8 +90,12 @@ class TestAPI(BaseTest):
 
                 data = {
                     "document_id": 1,
+                    "display_name": "Pablo Escobar",
                     "tag": "user_tag"
                 }
+                # Add record to ES before patching
+                self.es.index(index="users", id=data["document_id"], doc_type="document_id", body=data)
+
                 response = client.patch("/api/v1/users", data=data)
                 res = json.loads(response.data)
 
